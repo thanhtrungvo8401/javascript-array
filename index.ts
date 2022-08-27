ed95ff169e3827759be6984fb77b2cc9f55fecac
trungvt <thanhtrungvo8401@gmail.com>
2022-08-27 15:49:46 +0700

    lodash

--- index.ts
+++ index.ts
@@ -1,3 +1,37 @@
-const a: string = "Hello";
+import _ from "lodash";
 
-console.log(a);
+const arr = Array(100)
+    .fill(0)
+    .map((i: any) => Math.round(Math.random() * 100))
+    .sort((a, b) => a - b);
+//chunk
+// split array into many "chunks":
+// console.log(_.chunk(arr, 16));
+
+//compact
+// remove falsey value: (false, null, 0, "", undefined, and NaN are falsey)
+// console.log(_.compact([0, null, undefined, 1, "value", ""]))
+
+//different:
+// console.log(_.difference([1, 2, 3, 6], [3, 4, 1]));
+
+// Debounce:
+// let click = 0;
+// const triggerDebounce = _.debounce(() => { console.log("Debounce triggered")}, 1000);
+
+// const intervalId = setInterval(() => {
+//     console.log(click);
+//     click++;
+
+//     if (click > 10) {
+//         clearInterval(intervalId);
+//     }
+
+//     triggerDebounce();
+// }, 100);
+
+
+//deburr: 
+// remove special charactor:
+
+// console.log(_.deburr('xin chào, tiếng việt có dấu không được cho phép!, déjà vu'))
